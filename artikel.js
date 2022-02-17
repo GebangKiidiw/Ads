7064ba4b6f3523802568670ccb6a8c64ad114e9e
GebangKiidiw <46375292+GebangKiidiw@users.noreply.github.com>
2022-02-17 22:02:54 +0700

    Update artikel.js

--- artikel.js
+++ artikel.js
@@ -1,3 +1,4 @@
+<script type="text/javascript">
 	atOptions = {
 		'key' : 'ea5cebab5d06e1535e2775175f07b25b',
 		'format' : 'iframe',
@@ -6,3 +7,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://arousedimitateplane.com/ea5cebab5d06e1535e2775175f07b25b/invoke.js"></scr' + 'ipt>');
+</script>

cb68f00d362bf8357af1eb4d9b806d48de4768f0
GebangKiidiw <46375292+GebangKiidiw@users.noreply.github.com>
2022-02-17 15:54:30 +0700

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,6 +8,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.trustedcpmrevenue.com/jsutw9n4?key=fc13c94f072d24b5a57b8f81c11d12be', '_blank');
+	    window.open('https://arousedimitateplane.com/h4kphaus2g?key=acc1ca19b3d441426784c127b66511c2', '_blank');
 	});
 //]]>

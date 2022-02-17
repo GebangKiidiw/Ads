ee3788dbf28e8697a0787924977c939fc57e9c03
GebangKiidiw <46375292+GebangKiidiw@users.noreply.github.com>
2022-02-17 15:51:43 +0700

    Update artikel.js

--- artikel.js
+++ artikel.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '98997cebcc422dcf318d719442cb1a7d',
+		'key' : 'ea5cebab5d06e1535e2775175f07b25b',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/98997cebcc422dcf318d719442cb1a7d/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://arousedimitateplane.com/ea5cebab5d06e1535e2775175f07b25b/invoke.js"></scr' + 'ipt>');

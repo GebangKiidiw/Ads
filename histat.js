7fd410373aabfd9ef80370b44345b5d5c4ed4abf
GebangKiidiw <46375292+GebangKiidiw@users.noreply.github.com>
2022-02-19 04:37:11 +0700

    Update histat.js

--- histat.js
+++ histat.js
@@ -1,7 +1,6 @@
-<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
    <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4332654,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,4510154,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -9,5 +8,5 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4332654&101" alt="" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4510154&101" alt="" border="0"></a></noscript>
 <!-- Histats.com  END  -->

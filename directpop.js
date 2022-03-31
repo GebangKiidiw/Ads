36c4dbd455a098ae7d4c00afbc537ad3109ee7a8
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-04-01 05:04:03 +0700

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -12,15 +12,3 @@
 	    window.open('https://arousedimitateplane.com/h4kphaus2g?key=acc1ca19b3d441426784c127b66511c2', '_blank');
 	});
 </script>
-   <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4332654,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4332654&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->

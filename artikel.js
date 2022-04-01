000c36c04d595fdd661b86ee24a87840238fc735
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-04-01 10:17:52 +0700

    Delete artikel.js

--- artikel.js
+++
@@ -1,9 +0,0 @@
-	atOptions = {
-		'key' : 'ea5cebab5d06e1535e2775175f07b25b',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://arousedimitateplane.com/ea5cebab5d06e1535e2775175f07b25b/invoke.js"></scr' + 'ipt>');
-

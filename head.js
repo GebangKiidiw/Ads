4fc47256bf00f505efe102173c96a63404e8f123
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-04-01 10:18:14 +0700

    Delete head.js

--- head.js
+++
@@ -1,8 +0,0 @@
-	atOptions = {
-		'key' : '3aa6c5a146c61650f9afe29d8c1292c7',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://arousedimitateplane.com/3aa6c5a146c61650f9afe29d8c1292c7/invoke.js"></scr' + 'ipt>');

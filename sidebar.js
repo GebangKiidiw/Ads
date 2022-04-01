271ae03c9a12c708e7c6bbfd9ad5c024a12b61ad
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-04-01 10:19:43 +0700

    Delete sidebar.js

--- sidebar.js
+++
@@ -1,8 +0,0 @@
-	atOptions = {
-		'key' : 'b92ad093d3ff7c5d411cc66acc00fe84',
-		'format' : 'iframe',
-		'height' : 300,
-		'width' : 160,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://arousedimitateplane.com/b92ad093d3ff7c5d411cc66acc00fe84/invoke.js"></scr' + 'ipt>');

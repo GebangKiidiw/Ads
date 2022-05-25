b3958bdaba7084a78dfd97b09e18a1a8d9ddb394
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-05-25 15:28:18 +0700

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,7 +8,7 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
+	    window.open('https://arousedimitateplane.com/nxnu5jph?key=988b1fb26d3c8c3fcc01129564b60272','_blank');
 	});
 //]]>
 

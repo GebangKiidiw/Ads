7b682ae952e84fbff4ac12db27928cfd1fdffe9d
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2022-06-16 01:28:11 +0700

    Update birds.js

--- birds.js
+++ birds.js
@@ -1,4 +1,5 @@
-<script type="text/javascript">
+type='text/javascript'>
+//<![CDATA[
 	$(document).ready(function()
 	{
 	  $('body').addClass('xepo_ads');
@@ -7,6 +8,6 @@
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://arousedimitateplane.com/h4kphaus2g?key=acc1ca19b3d441426784c127b66511c2', '_blank');
+	    window.open('https://arousedimitateplane.com/h4kphaus2g?key=acc1ca19b3d441426784c127b66511c2','_blank');
 	});
-</script>
+//]]>

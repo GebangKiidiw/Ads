33f6801128274baf335122f407ff36ea732e3df1
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-03-25 07:55:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Jumlah uang yang akan jatuh
+const numMoney = 30;
+
+// Container untuk uang
+const moneyContainer = document.querySelector('.money-rain-container');
+
+// Fungsi untuk membuat uang jatuh secara acak
+function createMoney() {
+  const money = document.createElement('img');
+  money.src = 'https://static.vecteezy.com/system/resources/previews/028/766/359/original/dana-payment-icon-symbol-free-png.png'; // Ganti dengan path gambar uang Anda
+  money.alt = 'Money';
+  money.className = 'money';
+  money.style.left = `${Math.random() * window.innerWidth}px`; // Menentukan posisi horizontal secara acak
+  money.style.animationDuration = `${Math.random() * 3 + 2}s`; // Menentukan kecepatan jatuh secara acak (antara 2 dan 5 detik)
+  moneyContainer.appendChild(money);
+}
+
+// Membuat uang jatuh
+for (let i = 0; i < numMoney; i++) {
+  createMoney();
+}

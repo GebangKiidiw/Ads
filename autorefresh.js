ae4eab5a6359044bac1ff7f8e6ae5b03732307ec
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-19 14:45:21 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -1,27 +1,11 @@
-    // Daftar URL yang ingin Anda gunakan
-    var urls = [
-        "https://business.faridahdecoration.com/2023/07/crm-marketing-service-recovery-winning.html",
-        "https://business.faridahdecoration.com/2023/07/the-best-guide-to-finding-old-maximizer.html",
-        "https://business.faridahdecoration.com/2023/07/understanding-power-of-email-marketing.html"
-    ];
+var refreshCount = 0;
 
-    var currentIndex = 0; // Indeks halaman yang sedang ditampilkan
-
-    // Fungsi untuk memperbarui halaman berikutnya
-    function autoRefresh() {
-        // Periksa apakah sudah mencapai halaman terakhir
-        if (currentIndex === urls.length) {
-            console.log("Telah mencapai halaman terakhir. Berhenti auto-refresh.");
-            clearInterval(refreshInterval); // Hentikan auto-refresh
-        } else {
-            // Ambil URL halaman berikutnya
-            var nextPageUrl = urls[currentIndex];
-            // Redirect ke halaman berikutnya
-            window.location.href = nextPageUrl;
-            // Perbarui indeks halaman untuk mengarahkan ke halaman berikutnya
-            currentIndex++;
-        }
+function autoRefresh() {
+    if (refreshCount < 3) {
+        location.reload();
+        refreshCount++;
     }
+}
 
-    // Panggil fungsi autoRefresh setiap 15 detik
-    var refreshInterval = setInterval(autoRefresh, 15000);
+// Atur interval untuk melakukan refresh setiap 15 detik
+setInterval(autoRefresh, 15000);

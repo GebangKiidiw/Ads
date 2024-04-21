9381fc949e661abc56f5eac635eef47c02c1c414
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-21 08:07:45 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -1,13 +1,39 @@
-var refreshCount = 0;
+  function updateProgressBar(percentage) {
+    var progressBar = document.querySelector('.progress-bar');
+    progressBar.style.width = percentage + '%';
+    progressBar.innerText = percentage + '%';
+  }
 
-function autoRefresh() {
-    if (refreshCount < 3) {
-        setTimeout(function() {
-            location.reload();
-            refreshCount++;
-        }, 8000); // 15 detik
-    }
-}
+  function showButton() {
+    document.getElementById('button-container').style.display = 'block';
+  }
 
-// Panggil fungsi autoRefresh saat halaman dimuat
-window.onload = autoRefresh;
+  function hideButton() {
+    document.getElementById('button-container').style.display = 'none';
+  }
+
+  function resetProgress() {
+    var progressContainer = document.getElementById('progress-container');
+    progressContainer.innerHTML = ''; // Menghapus elemen progres
+    hideButton(); // Menyembunyikan tombol
+    startProgress(); // Memulai kembali progres
+  }
+
+  function startProgress() {
+    var currentPercentage = 0;
+    var interval = setInterval(function() {
+      currentPercentage += 5;
+      updateProgressBar(currentPercentage);
+      if (currentPercentage >= 100) {
+        clearInterval(interval);
+        showButton(); // Menampilkan tombol setelah mencapai 100%
+      }
+      if (currentPercentage === 100) {
+        var progressContainer = document.getElementById('progress-container');
+        progressContainer.style.display = 'none'; // Menyembunyikan elemen progres setelah mencapai 100%
+      }
+    }, 500); // Ubah 500 menjadi durasi yang diinginkan (dalam milidetik)
+  }
+
+  // Memulai progres saat halaman dimuat
+  startProgress();

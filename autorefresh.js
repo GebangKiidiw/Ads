e67eea608181c10c91455c7e626dbae866bbdb40
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-21 08:30:10 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -1,39 +1,41 @@
-  function updateProgressBar(percentage) {
+function updateProgressBar(percentage) {
     var progressBar = document.querySelector('.progress-bar');
     progressBar.style.width = percentage + '%';
     progressBar.innerText = percentage + '%';
-  }
+}
 
-  function showButton() {
-    document.getElementById('button-container').style.display = 'block';
-  }
+function showButton() {
+    document.getElementById('download-button').style.display = 'block'; // Mengubah ID menjadi 'download-button'
+}
 
-  function hideButton() {
-    document.getElementById('button-container').style.display = 'none';
-  }
+function hideButton() {
+    document.getElementById('download-button').style.display = 'none'; // Mengubah ID menjadi 'download-button'
+}
 
-  function resetProgress() {
+function resetProgress() {
     var progressContainer = document.getElementById('progress-container');
     progressContainer.innerHTML = ''; // Menghapus elemen progres
     hideButton(); // Menyembunyikan tombol
     startProgress(); // Memulai kembali progres
-  }
+}
 
-  function startProgress() {
+function startProgress() {
     var currentPercentage = 0;
     var interval = setInterval(function() {
-      currentPercentage += 5;
-      updateProgressBar(currentPercentage);
-      if (currentPercentage >= 100) {
-        clearInterval(interval);
-        showButton(); // Menampilkan tombol setelah mencapai 100%
-      }
-      if (currentPercentage === 100) {
-        var progressContainer = document.getElementById('progress-container');
-        progressContainer.style.display = 'none'; // Menyembunyikan elemen progres setelah mencapai 100%
-      }
+        currentPercentage += 5;
+        updateProgressBar(currentPercentage);
+        if (currentPercentage >= 100) {
+            clearInterval(interval);
+            hideProgressBar(); // Menyembunyikan elemen progres setelah mencapai 100%
+            showButton(); // Menampilkan tombol setelah mencapai 100%
+        }
     }, 500); // Ubah 500 menjadi durasi yang diinginkan (dalam milidetik)
-  }
+}
 
-  // Memulai progres saat halaman dimuat
-  startProgress();
+function hideProgressBar() {
+    var progressContainer = document.getElementById('progress-container');
+    progressContainer.style.display = 'none'; // Menyembunyikan elemen progres setelah mencapai 100%
+}
+
+// Memulai progres saat halaman dimuat
+startProgress();

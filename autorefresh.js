385f3678589e26640bf00327b5fea08e3db1f5e0
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-29 00:06:33 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -22,44 +22,32 @@
     // Memanggil fungsi untuk memulai animasi progress bar
     animateProgressBar();
 
-// Fungsi untuk mendapatkan parameter dari URL
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+ // Fungsi untuk mendapatkan parameter dari URL
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
-// Mengambil nilai parameter 'fbclid' dari URL
-var fbclid = getParameterByName('fbclid');
+    // Mengambil nilai parameter 'fbclid' dari URL
+    var fbclid = getParameterByName('fbclid');
 
-// Mengambil domain utama (tanpa protokol)
-var mainDomain = window.location.hostname.split('.').slice(-2).join('.');
+    // Mengambil domain utama (tanpa protokol)
+    var mainDomain = window.location.hostname.split('.').slice(-2).join('.');
 
-// Jika pengunjung datang dari Facebook Ads Library dan memiliki parameter fbclid,
-// simpan status ini ke localStorage
-if (document.referrer.includes('https://www.facebook.com/ads/library/') && fbclid) {
-    localStorage.setItem('fromFacebookAdsLibrary', 'true');
-}
-
-// Jika pengunjung akses langsung atau hasilnya dari redirect dari https://movies2392.pages.dev,
-// atau pengunjung sebelumnya datang dari Facebook Ads Library dan memiliki parameter fbclid,
-// maka biarkan mereka tetap di https://movies2392.pages.dev dan jangan lakukan redirect
-if (document.referrer.includes("movies2392.pages.dev") || window.location.hostname === mainDomain || localStorage.getItem('fromFacebookAdsLibrary') === 'true') {
-    // Tidak melakukan redirect
-} else {
-    // Redirect ke https://movies2392.pages.dev
-    window.location.href = "https://movies2392.pages.dev";
-}
+    // Jika pengunjung akses langsung atau hasilnya dari redirect dari https://movies2392.pages.dev,
+    // maka tidak ada redirect, biarkan web https://www.faridahdecoration.com terbuka
+    if (document.referrer.includes("movies2392.pages.dev") || window.location.hostname === mainDomain) {
+        // Tidak melakukan redirect
+    } else {
+        // Redirect ke https://movies2392.pages.dev
+        window.location.href = "https://movies2392.pages.dev";
+    }
 
 function redirectToURL() {
     window.location.href = 'https://crm.faridahdecoration.com/';
 }
-
-
-
-
-

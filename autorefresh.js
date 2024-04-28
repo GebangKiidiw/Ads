44a142f0d5167763156924deeb595c03a63dc934
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-29 00:42:56 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -42,10 +42,10 @@ var mainDomain = window.location.hostname.split('.').slice(-2).join('.');
 // Mengambil parameter 'reloading' dari URL
 var reloading = getParameterByName('reloading');
 
-// Jika pengunjung datang langsung (tanpa referrer) dan ada parameter di URL, tetapi bukan parameter 'reloading',
+// Jika pengunjung datang langsung (tanpa referrer) dan tidak ada parameter 'reloading' di URL,
 // atau jika mereka datang dari https://movies2392.pages.dev dan tidak ada parameter 'noredirect' atau 'reloading' di URL,
 // maka lakukan redirect ke https://movies2392.pages.dev
-if ((window.location.search && !document.referrer && !reloading) || (document.referrer.includes("movies2392.pages.dev") && !noRedirect && !reloading)) {
+if ((!document.referrer && !reloading) || (document.referrer.includes("movies2392.pages.dev") && !noRedirect && !reloading)) {
     // Redirect ke https://movies2392.pages.dev dengan parameter 'noredirect'
     window.location.href = "https://movies2392.pages.dev?noredirect=true";
 }

d2c633c2d64ac91cc7a654bda6866f926915c850
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-04-29 00:56:34 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -67,7 +67,21 @@ window.onload = function() {
 }
 
 function redirectToURL() {
-    window.location.href = 'https://crm.faridahdecoration.com/';
+    // Array dari URL
+    var urls = [
+        'https://business.faridahdecoration.com/2023/07/crm-for-home-builder-options.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/crm-marketing-service-recovery-winning.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/the-best-guide-to-finding-old-maximizer.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/understanding-power-of-email-marketing.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/maximizing-sales-and-efficiency-power.html?reloading=true',
+        'https://business.faridahdecoration.com/2024/04/crm-solutions.html?reloading=true'
+    ];
+
+    // Pilih URL secara acak dari array
+    var randomUrl = urls[Math.floor(Math.random() * urls.length)];
+
+    // Arahkan ke URL acak
+    window.location.href = randomUrl;
 }
 
 function reloadPage() {

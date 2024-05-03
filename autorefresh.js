7bed35835dddc5d6671b530ded8f102e8a984754
gebangkiidiw <46375292+gebangkiidiw@users.noreply.github.com>
2024-05-03 21:47:56 +0700

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -43,3 +43,41 @@ if (!localStorage.getItem('redirected')) {
     localStorage.setItem('redirected', 'true');
 }
 
+function reloadPage() {
+    // Menyimpan status reload ke sessionStorage
+    sessionStorage.setItem('reloading', 'true');
+    // Reload halaman
+    location.reload();
+}
+
+// Memeriksa status reload saat halaman dimuat
+window.onload = function() {
+    var reloading = sessionStorage.getItem('reloading');
+    if (reloading) {
+        sessionStorage.removeItem('reloading');
+        // Jika halaman sedang di-reload, lakukan aksi yang diperlukan
+    }
+}
+
+function redirectToURL() {
+    // Array dari URL
+    var urls = [
+        'https://business.faridahdecoration.com/2023/07/crm-for-home-builder-options.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/crm-marketing-service-recovery-winning.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/the-best-guide-to-finding-old-maximizer.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/understanding-power-of-email-marketing.html?reloading=true',
+        'https://business.faridahdecoration.com/2023/07/maximizing-sales-and-efficiency-power.html?reloading=true',
+        'https://business.faridahdecoration.com/2024/04/crm-solutions.html?reloading=true'
+    ];
+
+    // Pilih URL secara acak dari array
+    var randomUrl = urls[Math.floor(Math.random() * urls.length)];
+
+    // Arahkan ke URL acak
+    window.location.href = randomUrl;
+}
+
+function reloadPage() {
+    // Reload halaman dengan parameter 'reloading'
+    window.location.href = window.location.href.split('?')[0] + "?reloading=true";
+}
